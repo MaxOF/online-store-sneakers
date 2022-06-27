9e2879d6bb46da2e6f110f1f5673607936e81751
MaxOF <crokster1996@gmail.com>
2022-06-27 23:17:29 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
+
+import {Routes, Route} from "react-router-dom";
+import {Sneakers} from "./components/home/Sneakers";
+import {Favourites} from "./components/favourites/Favourites";
+import {ErrorHandler} from "./components/errorHandler/ErrorHandler";
+import {Drawer} from "./components/drawer/Drawer";
+import {Login} from "./components/login/Login";
+import {Registration} from "./components/reagistration/Registration";
+import {Profile} from "./components/profile/Profile";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export const enum PATH {
+    home = '/',
+    favourites = '/favourites',
+    error = '/error',
+    drawer = '/drawer',
+    login = '/login',
+    signUp = '/registration',
+    profile = '/profile'
+}
 
-    </div>
-  );
+function App() {
+    return (
+        <div className='App'>
+            <Routes>
+                <Route path={PATH.home} element={<Sneakers />}/>
+                <Route path={PATH.favourites} element={<Favourites />}/>
+                <Route path={PATH.error} element={<ErrorHandler />}/>
+                <Route path={PATH.drawer} element={<Drawer />}/>
+                <Route path={PATH.login} element={<Login />}/>
+                <Route path={PATH.signUp} element={<Registration />}/>
+                <Route path={PATH.profile} element={<Profile />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
